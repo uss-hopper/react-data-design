be1ce57e20fbf22f3f0fd32c4529fc2437033a5f
Dylan McDonald <dmcdonald21@cnm.edu>
2015-03-23 16:55:15 -0600

    Created a way of streamlining throwing exceptions when type hints fail

---
+++ php/lib/classcast.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Exception used for custom handling of type hinting errors
+ *
+ * Named after Java's venerable ClassCastException, this exception will be thrown when a type hint is invalid
+ *
+ * @author Dylan McDonald <dmcdonald21@cnm.edu>
+ * @see <http://docs.oracle.com/javase/8/docs/api/java/lang/ClassCastException.html>
+ * @see <http://php.net/manual/en/language.oop5.typehinting.php>
+ **/
+class ClassCastException extends ErrorException {}
+
+/**
+ * registers the throwing of a ClassCastException when type hinting fails
+ *
+ * @param int $errorNumber error code
+ * @param string $message error message
+ * @param string $file file the error was triggered in
+ * @param int $line line number the error was triggered on
+ * @return bool false when a ClassCastException isn't needed (and we revert to PHP's default error handling)
+ * @throws ClassCastException when a ClassCastException is neeed
+ **/
+function registerClassCastException($errorNumber, $message, $file, $line) {
+	// type hinting is always an E_RECOVERABLE_ERROR
+	if($errorNumber === E_RECOVERABLE_ERROR) {
+		throw(new ClassCastException($message, $errorNumber, 1, $file, $line));
+	}
+	// return false to defer to PHP's default error handling
+	return(false);
+}
+set_error_handler("registerClassCastException");
+?>
