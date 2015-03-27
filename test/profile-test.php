ec1a2f21ca46c45bfd5237f3dfa59a0da7d8327d
Eric Debelak <ericdebelak@gmail.com>
2015-03-27 11:30:40 -0600

    added asserts

--- test/profile-test.php
+++ test/profile-test.php
@@ -8,7 +8,11 @@ class ProfileTest extends GenericTest {
 		$numRows = $this->getConnection()->getRowCount("profile");
 		$profile = new Profile(null, "@phpunit", "test@phpunit.de", "+12125551212");
 		$profile->insert(self::$pdo);
+		$pdoProfile = Profile::getProfileByProfileId(self::$pdo, $profile->getProfileId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
+		$this->assertEquals($pdoProfile->getAtHandle(), "@phpunit");
+		$this->assertEquals($pdoProfile->getEmail(), "test@phpunit.de");
+		$this->assertEquals($pdoProfile->getPhone(), "+12125551212");
 	}
 }
 ?>
