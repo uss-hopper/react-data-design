b6f8dddac196a85585373c973fec2820ac9fc57b
Dylan McDonald <dmcdonald21@cnm.edu>
2015-03-31 09:23:59 -0600

    Added the tweet test

---
+++ test/tweet-test.php
@@ -0,0 +1,234 @@
+<?php
+// grab the project test parameters
+require_once("data-design-test.php");
+
+// grab the class under scrutiny
+require_once("../php/classes/profile.php");
+require_once("../php/classes/tweet.php");
+
+/**
+ * Full PHPUnit test for the Tweet class
+ *
+ * This is a complete PHPUnit test of the Tweet class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Tweet
+ * @author Dylan McDonald <dmcdonald21@cnm.edu>
+ **/
+class TweetTest extends DataDesignTest {
+	/**
+	 * content of the Tweet
+	 * @var string $VALID_TWEETCONTENT
+	 **/
+	protected $VALID_TWEETCONTENT = "PHPUnit test passing";
+	/**
+	 * content of the updated Tweet
+	 * @var string $VALID_TWEETCONTENT2
+	 **/
+	protected $VALID_TWEETCONTENT2 = "PHPUnit test still passing";
+	/**
+	 * timestamp of the Tweet; this starts as null and is assigned later
+	 * @var DateTime $VALID_TWEETDATE
+	 **/
+	protected $VALID_TWEETDATE = null;
+	/**
+	 * Profile that created the Tweet; this is for foreign key relations
+	 * @var Profile profile
+	 **/
+	protected $profile = null;
+
+	/**
+	 * create dependent objects before running each test
+	 **/
+	public final function setUp() {
+		// run the default setUp() method first
+		parent::setUp();
+
+		// create and insert a Profile to own the test Tweet
+		$this->profile = new Profile(null, "@phpunit", "test@phpunit.de", "+12125551212");
+		$this->profile->insert($this->getPDO());
+
+		// calculate the date (just use the time the unit test was setup...)
+		$this->VALID_TWEETDATE = new DateTime();
+	}
+
+	/**
+	 * test inserting a valid Tweet and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidTweet() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tweet");
+
+		// create a new Tweet and insert to into mySQL
+		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoTweet = Tweet::getTweetByTweetId($this->getPDO(), $tweet->getTweetId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
+		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
+	}
+
+	/**
+	 * test inserting a Tweet that already exists
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testInsertInvalidTweet() {
+		// create a Tweet with a non null tweet id and watch it fail
+		$tweet = new Tweet(DataDesignTest::INVALID_KEY, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a Tweet, editing it, and then updating it
+	 **/
+	public function testUpdateValidTweet() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tweet");
+
+		// create a new Tweet and insert to into mySQL
+		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+
+		// edit the Tweet and update it in mySQL
+		$tweet->setTweetContent($this->VALID_TWEETCONTENT2);
+		$tweet->update($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoTweet = Tweet::getTweetByTweetId($this->getPDO(), $tweet->getTweetId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT2);
+		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
+	}
+
+	/**
+	 * test updating a Tweet that already exists
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testUpdateInvalidTweet() {
+		// create a Tweet with a non null tweet id and watch it fail
+		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->update($this->getPDO());
+	}
+
+	/**
+	 * test creating a Tweet and then deleting it
+	 **/
+	public function testDeleteValidTweet() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tweet");
+
+		// create a new Tweet and insert to into mySQL
+		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+
+		// delete the Tweet from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$tweet->delete($this->getPDO());
+
+		// grab the data from mySQL and enforce the Tweet does not exist
+		$pdoTweet = Tweet::getTweetByTweetId($this->getPDO(), $tweet->getTweetId());
+		$this->assertNull($pdoTweet);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("tweet"));
+	}
+
+	/**
+	 * test deleting a Tweet that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testDeleteInvalidTweet() {
+		// create a Tweet and try to delete it without actually inserting it
+		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->update($this->getPDO());
+	}
+
+	/**
+	 * test inserting a Tweet and regrabbing it from mySQL
+	 **/
+	public function testGetValidTweetByTweetId() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tweet");
+
+		// create a new Tweet and insert to into mySQL
+		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoTweet = Tweet::getTweetByTweetId($this->getPDO(), $tweet->getTweetId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
+		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
+	}
+
+	/**
+	 * test grabbing a Tweet that does not exist
+	 **/
+	public function testGetInvalidTweetByTweetId() {
+		// grab a profile id that exceeds the maximum allowable profile id
+		$tweet = Tweet::getTweetByTweetId($this->getPDO(), DataDesignTest::INVALID_KEY);
+		$this->assertNull($tweet);
+	}
+
+	/**
+	 * test grabbing a Tweet by tweet content
+	 **/
+	public function testGetValidTweetByTweetContent() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tweet");
+
+		// create a new Tweet and insert to into mySQL
+		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Tweet::getTweetByTweetContent($this->getPDO(), $tweet->getTweetContent());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertEquals(count($results), 1);
+
+		// grab the result from the array and validate it
+		$pdoTweet = $results[0];
+		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
+		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
+	}
+
+	/**
+	 * test grabbing a Tweet by content that does not exist
+	 **/
+	public function testGetInvalidTweetByTweetContent() {
+		// grab a profile id that exceeds the maximum allowable profile id
+		$tweet = Tweet::getTweetByTweetContent($this->getPDO(), "nobody ever tweeted this");
+		$this->assertNull($tweet);
+	}
+
+	/**
+	 * test grabbing all Tweets
+	 **/
+	public function testGetAllValidTweets() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tweet");
+
+		// create a new Tweet and insert to into mySQL
+		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Tweet::getAllTweets($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertEquals(count($results), 1);
+
+		// grab the result from the array and validate it
+		$pdoTweet = $results[0];
+		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
+		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
+	}
+}
+?>
