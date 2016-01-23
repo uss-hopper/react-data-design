9d7deb5fe5c580c9b7a353045143f23e1cebad66
Dylan McDonald <dmcdonald21@cnm.edu>
2016-01-22 21:55:38 -0700

    Updated obsolete doc block

--- php/php70/classes/ValidateDate.php
+++ php/php70/classes/ValidateDate.php
@@ -16,7 +16,7 @@ trait ValidateDate {
 	 * Converts a string to a DateTime object or false if invalid. This is designed to be used within a mutator method.
 	 *
 	 * @param mixed $newDate date to validate
-	 * @return \DateTime|bool DateTime object containing the validated date or false if invalid
+	 * @return \DateTime DateTime object containing the validated date
 	 * @see http://php.net/manual/en/class.datetime.php PHP's DateTime class
 	 * @throws \InvalidArgumentException if the date is in an invalid format
 	 * @throws \RangeException if the date is not a Gregorian date
@@ -34,19 +34,19 @@ trait ValidateDate {
 		}
 
 		// verify the date is really a valid calendar date
-		$year   = intval($matches[1]);
-		$month  = intval($matches[2]);
-		$day	= intval($matches[3]);
-		$hour   = intval($matches[4]);
+		$year = intval($matches[1]);
+		$month = intval($matches[2]);
+		$day = intval($matches[3]);
+		$hour = intval($matches[4]);
 		$minute = intval($matches[5]);
 		$second = intval($matches[6]);
 		if(checkdate($month, $day, $year) === false) {
-			throw(new \RangeException("date $newDate is not a Gregorian date"));
+			throw(new \RangeException("date is not a Gregorian date"));
 		}
 
 		// verify the time is really a valid wall clock time
 		if($hour < 0 || $hour >= 24 || $minute < 0 || $minute >= 60 || $second < 0  || $second >= 60) {
-			throw(new \RangeException("date $newDate is not a valid time"));
+			throw(new \RangeException("date is not a valid wall clock time"));
 		}
 
 		// if we got here, the date is clean
