afe09cefe4382fe883584ce103420b0243804090
Dylan McDonald <dmcdonald21@cnm.edu>
2016-07-16 22:09:24 -0600

    Overrode the like table since LIKE is an SQL keyword

--- test/DataDesignTest.php
+++ test/DataDesignTest.php
@@ -45,7 +45,8 @@ abstract class DataDesignTest extends \PHPUnit_Extensions_Database_TestCase {
 		// THESE TABLES *MUST* BE LISTED IN THE SAME ORDER THEY WERE CREATED!!!!
 		$dataset->addTable("profile");
 		$dataset->addTable("tweet");
-//		$dataset->addTable("like");
+		// the second parameter is required because like is also a SQL keyword and is the only way PHPUnit can query the like table
+		$dataset->addTable("like", "SELECT likeProfileId, likeTweetId, likeDate FROM `like`");
 		return($dataset);
 	}
 
