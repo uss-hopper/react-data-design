484ee9e754fea7473e53dde1d766b3651869c271
Dylan McDonald <dmcdonald21@cnm.edu>
2016-07-16 19:58:06 -0600

    imported autoloader

---
+++ public_html/php/classes/autoload.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * PSR-4 Compliant Autoloader
+ *
+ * This will dynamically load classes by resolving the prefix and class name. This is the method that frameworks
+ * such as Laravel and Composer automatically resolve class names and load them. To use it, simply set the
+ * configurable parameters inside the closure. This example is taken from PHP-FIG, referenced below.
+ *
+ * @param string $class fully qualified class name to load
+ * @see http://www.php-fig.org/psr/psr-4/examples/ PSR-4 Example Autoloader
+ **/
+spl_autoload_register(function($class) {
+	/**
+	 * CONFIGURABLE PARAMETERS
+	 * prefix: the prefix for all the classes (i.e., the namespace)
+	 * baseDir: the base directory for all classes (default = current directory)
+	 **/
+	$prefix = "Edu\\Cnm\\Dmcdonald21\\DataDesign";
+	$baseDir = __DIR__;
+
+	// does the class use the namespace prefix?
+	$len = strlen($prefix);
+	if (strncmp($prefix, $class, $len) !== 0) {
+		// no, move to the next registered autoloader
+		return;
+	}
+
+	// get the relative class name
+	$className = substr($class, $len);
+
+	// replace the namespace prefix with the base directory, replace namespace
+	// separators with directory separators in the relative class name, append
+	// with .php
+	$file = $baseDir . str_replace("\\", "/", $className) . ".php";
+
+	// if the file exists, require it
+	if(file_exists($file)) {
+		require_once($file);
+	}
+});
