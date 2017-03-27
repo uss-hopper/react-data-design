de2eb039927a7a6b6ec16d0be6aa6d7cd09f97c0
George Kephart <Gkephart@cnm.edu>
2017-03-26 21:50:37 -0600

    Activation is written just needs testing

---
+++ public_html/api/activation/index.php
@@ -0,0 +1,85 @@
+
+<?php
+require_once dirname(__DIR__,3)."/php/classes/autoload.php";
+require_once dirname(__DIR__,3)."/php/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\Dmcdonald21\DataDesign\Profile;
+/**
+ * API to check profile activation status
+ * @author Gkephart
+ */
+// Check the session. If it is not active, start the session.
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try{
+	// grab the MySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/growify.ini");
+
+	//check the HTTP method being used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input (never trust the end user
+	$activation = filter_input(INPUT_GET, "activation", FILTER_SANITIZE_STRING);
+
+	// make sure the activation token is the correct size
+	if(strlen($activation) !== 16){
+		throw(new InvalidArgumentException("activation has an incorrect length", 405));
+	}
+
+	// verify that the activation tokens value is a string hexadeciaml value
+	if(ctype_xdigit($activation) === false) {
+		throw (new \InvalidArgumentException("activation is empty or has invalid contents", 405));
+	}
+	// handle The GET HTTP request
+	if($method === "GET"){
+
+		// set XSRF Cookie
+		setXsrfCookie();
+
+		//find profile associated with the activation token
+		$profile = Profile::getProfileByProfileActivation($pdo, $activation);
+
+		//verify the profile is not null
+		if($profile !== null){
+
+			//make sure the activation token matches
+			if($activation === $profile->getProfileActivation()) {
+
+				//set activation to null
+				$profile->setProfileActivation(null);
+
+				//update the profile in the database
+				$profile->update($pdo);
+
+				//set the reply for the end user
+				$reply->data = "Thank you for activating your account, you will be auto-redirected to your profile shortly.";
+			}
+		} else {
+			//throw an exception if the activation token does not exist
+			throw(new RuntimeException("Profile with this activation code does not exist", 404));
+		}
+	} else {
+		//throw an exception if the HTTP request is not a GET
+		throw(new InvalidArgumentException("Invalid HTTP method request"));
+	}
+
+	//update the reply objects status and message state variables if an exception or type exception was thrown;
+} catch (Exception $exception){
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError){
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+
+//prepare and send the reply
+header("Content-type: application/json");
+if($reply->data === null){
+	unset($reply->data);
+}
+echo json_encode($reply);
