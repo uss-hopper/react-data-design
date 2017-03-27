d0352c97271e3bc0e4571740b611d4fd12e78107
George Kephart <Gkephart@cnm.edu>
2017-03-26 22:34:22 -0600

    done for the night sign in is pretty close to finished.

---
+++ public_html/api/sign-in/index.php
@@ -0,0 +1,82 @@
+<?php
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\Dmcdonald21\DataDesign\Profile;
+
+/**
+ * api for handling sign-in
+ *
+ * @author Gkephart
+ **/
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+
+	//start session
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
+	//grab mySQL statement
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/sprout-swap.ini");
+
+	//determine which HTTP method is being used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//If method is post handle the sign in logic
+	else if($method === "POST") {
+
+		//make sure the XSRF Token is valid
+		verifyXsrf();
+
+		//process the request content and decode the json object into a php object
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//check to make sure the password and email field is not empty and are secure
+		if(empty($requestObject->profileEmail) === true) {
+			throw(new \InvalidArgumentException("Wrong email address.", 401));
+		} else {
+			$profileEmail = filter_var($requestObject->profileEmail, FILTER_SANITIZE_EMAIL);
+		}
+
+		if(empty($requestObject->profilePassword) === true) {
+			throw(new \InvalidArgumentException("Must enter a password."));
+		} else {
+		 	$profilePassword = $requestObject->profilePassword;
+		}
+
+		//grab the profile from the database by the email provided
+		$profile = Profile::getProfileByProfileEmail($pdo, $profileEmail);
+		if(empty($profile) === true) {
+			throw(new \InvalidArgumentException("Invalid Email"));
+		}
+
+		//hash the password given to make sure it matches.
+		$hash = hash_pbkdf2("sha512", $profilePassword, $profile->getProfileSalt(), 262144);
+
+		//verify hash is correct
+		if($hash !== $profile->getProfilePasswordHash()) {
+			throw(new \InvalidArgumentException("Password or email is incorrect."));
+		}
+
+		//grab profile from database and put into a session
+		$profile = Profile::getProfileByProfileId($pdo, $profile->getProfileId());
+		$_SESSION["profile"] = $profile;
+		$reply->message = "Sign in was successful.";
+	} else {
+		throw(new \InvalidArgumentException("Invalid HTTP method request."));
+	}
+
+	// if an exception is thrown update the
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+header("Content-type: application/json");
+echo json_encode($reply);
