dc2e027bef28d58fb992929b39308f5afbf062d7
Dylan McDonald <dmcdonald21@cnm.edu>
2017-03-28 16:00:54 -0600

    renamed to actual file name for the app module :joy_cat:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
-import {AppComponent} from "./app.component";
+import {AppComponent} from "./app.module";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 
 const moduleDeclarations = [AppComponent];
