91a5217e9b30605b2d5006d7d8bcd90b03e45585
Dylan McDonald <dmcdonald21@cnm.edu>
2017-03-28 15:59:10 -0600

    added app component

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
+
+const moduleDeclarations = [AppComponent];
+
+@NgModule({
+	imports:      [BrowserModule, FormsModule, HttpModule, routing],
+	declarations: [...moduleDeclarations, ...allAppComponents],
+	bootstrap:    [AppComponent],
+	providers:    [appRoutingProviders]
+})
+export class AppModule {}
