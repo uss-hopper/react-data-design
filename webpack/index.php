5db39fd53d4da92b79dcdca21aae4d0c3289b35e
Dylan McDonald <dmcdonald21@cnm.edu>
2017-03-28 15:51:40 -0600

    added master webpack template

---
+++ webpack/index.php
@@ -0,0 +1,19 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<base href="/" />
+
+		<title>Twitter Data Design Example</title>
+	</head>
+	<body>
+		<data-design-app>Loading&hellip;</data-design-app>
+	</body>
+</html>
