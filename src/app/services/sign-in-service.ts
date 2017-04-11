f85ba77606d85c035bf7e32d0bc962a495c15b93
George Kephart <Gkephart@cnm.edu>
2017-04-11 14:25:40 -0600

    sign-in service is complete

---
+++ src/app/services/sign-in-service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {BaseService} from "./base-service";
+import {Status} from "../classes/status";
+import {SignIn} from "../classes/sign-in";
+
+@Injectable()
+export class SignInService extends BaseService {
+	constructor(protected http:Http) {
+		super(http);
+	}
+
+	private signInUrl = "api/sign-in/";
+
+	//preform the post to initiate sign in
+	postSignIn(signIn:SignIn) : Observable<Status> {
+		return(this.http.post(this.signInUrl, signIn)
+			.map(BaseService.extractMessage)
+			.catch(BaseService.handleError));
+	}
+}
