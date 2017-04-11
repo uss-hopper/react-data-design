c12fc83c6dedc22140f34392dc9e055fb20cd9c4
George Kephart <Gkephart@cnm.edu>
2017-04-11 10:32:50 -0600

    made a small change to base service I had to make the methods static. this change might have to be fixed. Also still cranking away at the profile service

--- src/app/services/base-service.ts
+++ src/app/services/base-service.ts
@@ -1,6 +1,6 @@
 import {Http, Response} from "@angular/http";
 import {Observable} from "rxjs/Observable";
-import {Status} from "../class/status";
+import {Status} from "../classes/status";
 
 export abstract class BaseService {
 	constructor(protected http: Http) {}
@@ -17,7 +17,7 @@ export abstract class BaseService {
 		return(json.data);
 	}
 
-	protected extractMessage(response: Response) : Status {
+	protected static extractMessage(response: Response) : Status {
 		if(response.status < 200 || response.status >= 300) {
 			throw(new Error("Bad response status: " + response.status))
 		}
@@ -31,19 +31,7 @@ export abstract class BaseService {
 		return(status);
 	}
 
-	protected extractTomWu(response: Response) : any {
-		if(response.status < 200 || response.status >= 300) {
-			throw(new Error("Bad response status: " + response.status))
-		}
-
-		let json = response.json();
-		if(json.status !== 200) {
-			throw(new Error("Bad API status: " + json.status));
-		}
-		return(json);
-	}
-
-	protected handleError(error:any) {
+	protected static handleError(error:any) {
 		let message = error.message;
 		console.log(message);
 		return(Observable.throw(message));
--- src/app/services/profile-service.ts
+++ src/app/services/profile-service.ts
@@ -1,3 +1,33 @@
 import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
-import {}
+import {BaseService} from "./base-service";
+import {Status} from "../classes/status";
+import {Profile} from "../classes/profile";
+import {Observable} from "rxjs/Observable";
+
+@Injectable ()
+export class ProfileService extends BaseService {
+
+	constructor(protected http:Http) {
+		super(http);
+	}
+
+	//define the API endpoint
+	private profileUrl = "api/profile/";
+
+	//reach out to the profile  API and delete the profile in question
+	deleteProfile(id: number) : Observable<Status> {
+		return(this.http.delete(this.profileUrl + id)
+			.map(BaseService.extractMessage)
+			.catch(BaseService.handleError));
+	}
+
+	//call to the profile API and create the profile
+	createProfile(profile: Profile) : Observable<Status> {
+		return(this.http.post(this.profileUrl, profile)
+			.map(BaseService.extractMessage)
+			.catch(BaseService.handleError));
+	}
+
+	// call the the Profile API and create
+}
