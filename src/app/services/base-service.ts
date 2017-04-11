eb3cc2c1619466e98c510679c359bb9bfd01db64
George Kephart <Gkephart@cnm.edu>
2017-04-11 09:37:52 -0600

    created base service and am starting to work on profile service

---
+++ src/app/services/base-service.ts
@@ -0,0 +1,51 @@
+import {Http, Response} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {Status} from "../class/status";
+
+export abstract class BaseService {
+	constructor(protected http: Http) {}
+
+	protected extractData(response: Response) : any {
+		if(response.status < 200 || response.status >= 300) {
+			throw(new Error("Bad response status: " + response.status))
+		}
+
+		let json = response.json();
+		if(json.status !== 200) {
+			throw(new Error("Bad API status: " + json.status));
+		}
+		return(json.data);
+	}
+
+	protected extractMessage(response: Response) : Status {
+		if(response.status < 200 || response.status >= 300) {
+			throw(new Error("Bad response status: " + response.status))
+		}
+
+		let json = response.json();
+		let jsonStatus = "alert-success";
+		if(json.status !== 200) {
+			jsonStatus = "alert-danger";
+		}
+		let status = new Status(json.status, json.message, jsonStatus);
+		return(status);
+	}
+
+	protected extractTomWu(response: Response) : any {
+		if(response.status < 200 || response.status >= 300) {
+			throw(new Error("Bad response status: " + response.status))
+		}
+
+		let json = response.json();
+		if(json.status !== 200) {
+			throw(new Error("Bad API status: " + json.status));
+		}
+		return(json);
+	}
+
+	protected handleError(error:any) {
+		let message = error.message;
+		console.log(message);
+		return(Observable.throw(message));
+	}
+}
---
+++ src/app/services/profile-service.ts
@@ -0,0 +1,3 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {}
