a1a0c39eff0720ed7d1adc3b0382665a3656a81a
George Kephart <Gkephart@cnm.edu>
2017-06-02 17:44:11 -0600

    updated documentation for the mocking of sessions for testing databases

--- public_html/api/tweet/index.php
+++ public_html/api/tweet/index.php
@@ -32,9 +32,16 @@ try {
 	//grab the mySQL connection
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/ddctwitter.ini");
 
-	// mock a logged in user by mocking the session and assigning a specific user to it.
-	// this is only for testing purposes and should not be in the live code.
-	//$_SESSION["profile"] = Profile::getProfileByProfileId($pdo, 732);
+	/*
+	 * // mock a logged in user by forcing the session. This is only for testing purposes and should not be in the live code.
+	 *
+	 * // profileId of profile to use for testing,
+	 * $person = 2
+	 *
+	 * // grab a profile by its profileId and add it to the session
+	 * $_SESSION["profile"] = Profile::getProfileByProfileId($pdo, $person);
+	 *
+	 */
 
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
