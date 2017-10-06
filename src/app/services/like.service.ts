d732c984d9bd5604efe4fc1d621975a545d848b8
George Kephart <Gkephart@cnm.edu>
2017-10-05 20:39:43 -0600

    Began updating ddc-twitter to use angular 4.3 this should be a long process

--- src/app/services/like.service.ts
+++ src/app/services/like.service.ts
@@ -1,45 +1,32 @@
 import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
-import {BaseService} from "./base.service";
+import {HttpClient} from "@angular/common/http";
 import {Status} from "../classes/status";
 import {Like} from "../classes/like";
 import {Observable} from "rxjs/Observable";
 
 @Injectable ()
-export class LikeService extends BaseService {
+export class LikeService {
 
-	constructor(protected http:Http ) {
-		super(http);
+	constructor(protected http : HttpClient ) {
+
 	}
 
 	//define the API endpoint
 	private likeUrl = "/api/like/";
 
-	//call to the like API and delete the like in question
-	deleteLike(like :Like) : Observable<Status> {
-		return(this.http.put(this.likeUrl + like.likeProfileId +like.likeTweetId, like)
-			.map(this.extractData)
-			.catch(this.handleError));
-	}
 
 	//call the like API and create a new like
 	createLike(like : Like) : Observable<Status> {
-		return(this.http.post(this.likeUrl, like)
-			.map(this.extractMessage)
-			.catch(this.handleError));
+		return (this.http.post<Status>(this.likeUrl, like));
 	}
 
 	//grabs a spcfic like based on its composite key
-	getLikeByCompositeKey(likeProfileId : number, likeTweetId : number) : Observable <Like> {
-		return(this.http.get(this.likeUrl + likeProfileId + likeTweetId)
-			.map(this.extractData)
-			.catch(this.handleError));
+	getLikeByCompositeKey(likeProfileId : string, likeTweetId : string) : Observable <Like> {
+		return (this.http.get<Like>(this.likeUrl+ "?likeProfileId=" + likeProfileId +"&likeTweetId=" + likeTweetId))
 	}
 
-	getLikeBytweetId (likeTweetId : number ) : Observable <Like[]> {
-		return(this.http.get(this.likeUrl + likeTweetId)
-			.map(this.extractData)
-			.catch(this.handleError));
+	getLikeBytweetId (likeTweetId : string ) : Observable<Like[]> {
+	return(this.http.get<Like[]>(this.likeUrl + likeTweetId))
 	}
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { AppModule } from "./app/app.module";
-import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
 
-//enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule);
+window.addEventListener("DOMContentLoaded", event => {
+	// enableProdMode();
+	const platform = platformBrowserDynamic();
+	platform.bootstrapModule(AppModule);
+});
