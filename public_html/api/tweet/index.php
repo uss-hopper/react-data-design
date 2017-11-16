4d3d0074c484ca894dc2aee5341eea42f382f0fb
George Kephart <Gkephart@cnm.edu>
2017-11-15 23:35:57 -0700

    I almost have ddc-twitter refactored to use bootstarp4. I just need to refactor create-tweet and the display-tweet

--- public_html/api/tweet/index.php
+++ public_html/api/tweet/index.php
@@ -3,7 +3,7 @@
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
-require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\DataDesign\{
