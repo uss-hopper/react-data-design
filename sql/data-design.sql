bb2fb29695ca3b5b4c73d394a9ce3c0470b2ca61
George Kephart <Gkephart@cnm.edu>
2018-01-18 14:05:37 -0700

    cloudinary added again.

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -4,9 +4,9 @@
 -- never ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever
 -- do this on live data!!!!
 DROP TABLE IF EXISTS `like`;
+DROP TABLE IF EXISTS image;
 DROP TABLE IF EXISTS tweet;
 DROP TABLE IF EXISTS profile;
-
 -- the CREATE TABLE function is a function that takes tons of arguments to layout the table's schema
 CREATE TABLE profile (
 	-- this creates the attribute for the primary key
@@ -15,19 +15,18 @@ CREATE TABLE profile (
 	profileId BINARY(16) NOT NULL,
 	profileActivationToken CHAR(32),
 	profileAtHandle VARCHAR(32) NOT NULL,
+	profileCloudinaryToken VARCHAR(255),
 	-- to make sure duplicate data cannot exist, create a unique index
-	profileAvatarUrl VARCHAR(255) NOT NULL,
 	profileEmail VARCHAR(128) NOT NULL,
 	-- to make something optional, exclude the not null
-	profileHash	CHAR(128) NOT NULL,
+	profileHash CHAR(128) NOT NULL,
 	profilePhone VARCHAR(32),
 	profileSalt CHAR(64) NOT NULL,
 	UNIQUE(profileEmail),
 	UNIQUE(profileAtHandle),
 	-- this officiates the primary key for the entity
 	PRIMARY KEY(profileId)
-);
-
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 -- create the tweet entity
 CREATE TABLE tweet (
 	-- this is for yet another primary key...
@@ -43,8 +42,18 @@ CREATE TABLE tweet (
 	FOREIGN KEY(tweetProfileId) REFERENCES profile(profileId),
 	-- and finally create the primary key
 	PRIMARY KEY(tweetId)
-);
-
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+-- create the tweetImage entity
+CREATE TABLE image (
+	imageId BINARY(16) NOT NULL,
+	imageTweetId BINARY(16) NOT NULL,
+	imageCloudinaryToken VARCHAR(255) NOT NULL,
+	imageUrl VARCHAR(128) NOT NULL ,
+	INDEX(imageId),
+	INDEX(imageTweetId),
+	FOREIGN KEY(imageTweetId) REFERENCES tweet(tweetId),
+	PRIMARY KEY (imageId)
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 -- create the like entity (a weak entity from an m-to-n for profile --> tweet)
 CREATE TABLE `like` (
 	-- these are not auto_increment because they're still foreign keys
@@ -59,4 +68,4 @@ CREATE TABLE `like` (
 	FOREIGN KEY(likeTweetId) REFERENCES tweet(tweetId),
 	-- finally, create a composite foreign key with the two foreign keys
 	PRIMARY KEY(likeProfileId, likeTweetId)
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
