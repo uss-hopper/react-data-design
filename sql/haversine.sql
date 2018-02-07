702602912a911a4a2d2eac294526103a506f8255
Dylan McDonald <dmcdonald21@cnm.edu>
2018-02-07 09:54:41 -0700

    refactored to drop the point class

--- sql/haversine.sql
+++ sql/haversine.sql
@@ -1,15 +1,15 @@
--- @author Dylan McDonald
--- @see https://github.com/dylan-mcdonald/data-design/blob/master/sql/haversine.sql#L35
--- adding a comment
+-- @author Dylan McDonald <dmcdonald21@cnm.edu>
 
 -- drop the procedure if already defined
 DROP FUNCTION IF EXISTS haversine;
 
 -- procedure to calculate the distance between two points
--- @param POINT $origin point of origin
--- @param POINT $destination point heading out
--- @return DECIMAL distance between the points, in kilometers
-CREATE FUNCTION haversine(origin POINT, destination POINT) RETURNS FLOAT
+-- @param FLOAT $originX point of origin, x coordinate
+-- @param FLOAT $originY point of origin, y coordinate
+-- @param FLOAT $destinationX point heading out, x coordinate
+-- @param FLOAT $destinationY point heading out, y coordinate
+-- @return FLOAT distance between the points, in kilometers
+CREATE FUNCTION haversine(originX FLOAT, originY FLOAT, destinationX FLOAT, destinationY FLOAT) RETURNS FLOAT
 	BEGIN
 		-- first, declare all variables; I don't think you can declare later
 		DECLARE radius FLOAT;
@@ -23,10 +23,10 @@ CREATE FUNCTION haversine(origin POINT, destination POINT) RETURNS FLOAT
 
 		-- assign the variables that were declared & use them
 		SET radius = 3939; -- radius of the earth in miles
-		SET latitudeAngle1 = RADIANS(Y(origin));
-		SET latitudeAngle2 = RADIANS(Y(destination));
-		SET latitudePhase = RADIANS(Y(destination) - Y(origin));
-		SET longitudePhase = RADIANS(X(destination) - X(origin));
+		SET latitudeAngle1 = RADIANS(originX);
+		SET latitudeAngle2 = RADIANS(destinationY);
+		SET latitudePhase = RADIANS(destinationY - originY);
+		SET longitudePhase = RADIANS(destinationX - originX);
 
 		SET alpha = POW(SIN(latitudePhase / 2), 2)
 						+ POW(SIN(longitudePhase / 2), 2)
