3d2468c007752a6d3a2946b853cd6dbb80d40aee
Dylan McDonald <dmcdonald21@cnm.edu>
2018-03-06 09:32:27 -0700

    updated common to use ejs files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -63,7 +63,7 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			inject: "head",
 			filename: helpers.root("public_html") + "/index.html",
-			template: helpers.root("webpack") + "/index.html"
+			template: helpers.root("webpack") + "/index.ejs"
 		})
 	]
 };
