407bb14b7ee2e16d253a9e82362f96ce3b0b06dc
GKephart <gkephart@cnm.edu>
2018-08-15 14:59:23 -0600

    cleaned up helpers.js and did a slight rearrange of the package.json

--- webpack/helpers.js
+++ webpack/helpers.js
@@ -1,7 +1,6 @@
-var path = require("path");
-var _root = path.resolve(__dirname, "..");
+const path = require("path");
+const _root = path.resolve(__dirname, "..");
 function root(args) {
-	console.log(args);
 	args = Array.prototype.slice.call(arguments, 0);
 	return path.join.apply(path, [_root].concat(args));
 }
