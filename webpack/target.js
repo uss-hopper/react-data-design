c2c72a8d77a95c82276e94b910af3d4e2d1ce50c
George Kephart <Gkephart@cnm.edu>
2018-12-04 15:17:14 -0700

    updated my target.js

--- webpack/target.js
+++ webpack/target.js
@@ -1,4 +1,4 @@
 module.exports = function() {
 	//path to your deployment API directory
-	return basepath = "https://bootcamp-coders.cnm.edu/~gkephart/ddc-twitter/public_html/"; //Your URL here
+	return basepath = "https://bootcamp-coders.cnm.edu/~username/project/public_html/"; //Your URL here
 };
