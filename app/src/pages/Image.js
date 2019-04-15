308892c5b2efa59fd5f45197bc346509d41e93fb
george <gkephart@cnm.edu>
2019-04-15 16:10:42 -0600

    added react router and created my three initial pages.

---
+++ app/src/pages/Image.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class Image extends React.Component {
+	render() {
+		return <h1>Image</h1>
+	}
+}
+export default Image;
---
+++ app/src/pages/Profile.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class Profile extends React.Component {
+	render() {
+		return <h1>Profile</h1>
+	}
+}
+export default Profile
