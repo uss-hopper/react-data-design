dbd65017bbb98bd8b5cf4ec5889a0373e5582da9
george <gkephart@cnm.edu>
2019-04-15 18:24:29 -0600

    dont know if react bootstrap is going to make the cut.

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
 import Home from "./pages/Home";
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Profile from "./pages/Profile";
+import Image from "./pages/Image"
+
+
+
+const routing = (
+	<Router>
+		<div>
+			<Route exact path="/" component={Home} />
+			<Route path="/profile" component={Profile} />
+			<Route path="/image" component={Image} />
+		</div>
+	</Router>
+);
+
 
 
 ReactDOM.render(
-	<Home/>,
+	routing,
 	document.querySelector("#root")
 );
 
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react'
+import Card from "react-bootstrap/Card";
+import {Button} from "react-bootstrap/Button";
+
 class Home extends Component {
 	render() {
-		return <h1>Home</h1>
+		return (
+			<>
+				<Card>
+					<Card.Body>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of
+							the card's content.
+						</Card.Text>
+						<Button variant="primary">Go somewhere</Button>
+					</Card.Body>
+				</Card>
+			</>
+		);
 	}
-
 }
+
 export default Home;
