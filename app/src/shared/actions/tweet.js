25e12b9777c587a1551e4360a1e7978bc8110779
george <gkephart@cnm.edu>
2019-10-01 13:57:45 -0600

    removed debug code

--- app/src/shared/actions/tweet.js
+++ app/src/shared/actions/tweet.js
@@ -1,6 +1,6 @@
 import {httpConfig} from "../utils/http-config";
 
-export const getAllTweets = (test) => async (dispatch) => {
+export const getAllTweets = () => async (dispatch) => {
 	const payload =  await httpConfig.get("/apis/tweet/");
 	dispatch({type: "FETCH_TWEETS",payload : payload.data });
 };
